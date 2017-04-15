e6978cfd9082dd4c949a5cb7305c397a14b756ad
Arnaud Prat <arnaud.prat@gmail.com>
2017-04-15 16:25:46 +0200

    Add react-router and basic components

---
+++ catch-of-the-day/src/components/Order.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Order extends React.Component {
+	render() {
+		return (
+			<p>order</p>
+		)
+	}
+}
+
+export default Order;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,25 @@
-// let's go!
+// Libs, utils & helpers
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter, Match, Miss } from 'react-router';
+import './css/style.css';
+
+// Components
+import App from './components/App';
+import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
+
+const Root = () => {
+	return (
+		<BrowserRouter>
+			<div>
+				<Match exactly pattern="/" component={StorePicker} />
+				<Match pattern="/store/:storeId" component={App} />
+				<Miss component={NotFound} />
+			</div>
+		</BrowserRouter>
+	)
+}
+
+render(<Root/>, document.querySelector('#main'));
+
